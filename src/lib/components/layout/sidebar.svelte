805c4315ddc38501e907db1e0262e595567f04ff
VirtualizzeAI <virtualizzeautomacao@gmail.com>
2025-12-22 15:24:45 -0300

    troca de nome no menu: conexões > números

--- src/lib/components/layout/sidebar.svelte
+++ src/lib/components/layout/sidebar.svelte
@@ -97,7 +97,7 @@
         ariaLabel: 'Disparo em Massa'
       },
       {
-        name: 'Conexões',
+        name: 'Números',
         path: '/conexoes',
         icon: 'M12 18h.01M8 21h8a2 2 0 002-2V5a2 2 0 00-2-2H8a2 2 0 00-2 2v14a2 2 0 002 2z',
         ariaLabel: 'Conexões'
